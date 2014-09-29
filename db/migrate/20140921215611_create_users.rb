2611e46e0a1cc12985ba57c1cc92f84f75701214
Andrew Claxton <andrew.claxton@usma.edu>
2014-09-29 14:42:42 -0400

    user param problems

--- db/migrate/20140921215611_create_users.rb
+++ db/migrate/20140921215611_create_users.rb
@@ -3,8 +3,9 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-
-      t.timestamps
+	  t.string :password
+	  t.string :password_confirmation
+      
     end
   end
 end
