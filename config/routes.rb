b3cf9197e27d98a0fa76ef2c5e3b2ba105d2bbf7
Andrew Claxton <andrew.claxton@usma.edu>
2014-09-28 21:51:44 -0400

    files adjusted

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-``resources :users
+  resources :users
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
