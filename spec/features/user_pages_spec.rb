68e28f5fcca3741b364ce3ca35e18ec83d3bd75d
Andrew Claxton <andrew.claxton@usma.edu>
2014-09-29 13:48:01 -0400

    sign up form

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "UserPages", :type => :request do
 	expect(response).to have_http_status(200)
 	end
   end
-end
+
  
  describe "User pages" do
 
@@ -23,6 +23,32 @@ end
 		expect(page).to have_title "#{base_title} | Sign Up"
 	end
   end
+  describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+	end 
+
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
@@ -32,4 +58,5 @@ end
   end
 
 
+ 
 end
