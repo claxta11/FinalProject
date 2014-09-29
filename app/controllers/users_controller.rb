f990a102711b3b64a82156cf453e8512abdbe73e
Andrew Claxton <andrew.claxton@usma.edu>
2014-09-29 16:17:38 -0400

    users problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
    def show
     @user = User.find(params[:id])
-  end
+   end
 
   def new
 	@user = User.new
