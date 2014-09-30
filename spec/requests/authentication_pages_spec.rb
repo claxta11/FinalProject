5d4f7fe4bb316d3020a3fafd300f4da84cb1dd49
Andrew Claxton <andrew.claxton@usma.edu>
2014-09-29 20:24:42 -0400

    signin changes

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,11 +4,25 @@ describe "Authentication" do
 
   subject { page }
 
-  describe "signin page" do
+    
+     describe "signin" do
+
     before { visit signin_path }
 
-    it { should have_content('Sign in') }
-    it { should have_title('Sign in') }
-  end
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+    end
+
+
+
 end
 
+end 
